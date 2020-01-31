d2c92450209e932ab6e808b273136e85fe5adaf8
student <student@users-MacBook-Pro.local>
2020-01-30 16:24:03 -0800

    setting up homepage

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
- 
+
 var app = express();
- 
+require('./controllers/posts.js')(app);
+
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
  
 app.get('/', function (req, res) {
     res.render('home');
 });
+
+app.get('/posts/new', function(rew, res){
+    res.render('posts-new');
+})
  
 app.listen(3000);
