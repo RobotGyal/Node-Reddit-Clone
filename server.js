50e215781b3601a8415f6b7c423cee054df53da4
aleia <student@users-MacBook-Pro.local>
2020-02-03 14:59:03 -0800

    part 2

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var exphbs  = require('express-handlebars');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 var app = express();
 require('./controllers/posts.js')(app);
@@ -15,4 +17,15 @@ app.get('/posts/new', function(rew, res){
     res.render('posts-new');
 })
  
+
+// Use Body Parser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add after body parser initialization!
+app.use(expressValidator());
+
+// Set db
+require('./data/reddit-db');
+
 app.listen(3000);
