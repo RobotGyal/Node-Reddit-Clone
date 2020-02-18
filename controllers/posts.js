1ba6d218cd98e69a6f7c01be33533a40b19af324
aleia <student@users-MacBook-Pro.local>
2020-02-17 16:34:53 -0800

    completed part 3

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,30 +1,48 @@
 const Post = require('../models/post');
 
 module.exports = (app) => {
-  // GET: Create a new post page
-  app.get("/posts/new", (req, res) => {
-    res.render('posts-new', {});
-  });
-
-  app.post('/posts/new', (req, res) => {
-    console.log(req.body.title);
-    var post = new Post(req.body);
-
-    post.save().then((post) => {
-      return res.redirect('/')
-    }).catch((err) => {
-      console.log(err.message)
-    })
-
+  // Home page
+  app.get('/', (req, res) => {
     Post.find({})
       .then(posts => {
-        res.render("posts-index", 
-        {posts}
-        );
+        console.log(posts)
+        res.render("posts-index", {
+          posts
+        });
       })
       .catch(err => {
         console.log(err.message);
       });
   });
+
+
+  // GET: Create a new post page
+  app.get("/posts/new", (req, res) => {
+    res.render('posts-new', {});
+  });
+
+
+  // CREATE
+  app.post('/posts/new', (req, res) => {
+    // INSTANTIATE INSTANCE OF POST MODEL
+    const post = new Post(req.body);
+
+    // SAVE INSTANCE OF POST MODEL TO DB
+    post.save((err, post) => {
+      // REDIRECT TO THE ROOT
+      return res.redirect(`/`);
+    })
+  });
+
+app.get("/posts/:id", function(req, res) {
+  // LOOK UP THE POST
+  Post.findById(req.params.id)
+    .then(post => {
+      res.render("posts-show", { post });
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
 
 };
--- server.js
+++ server.js
@@ -1,4 +1,9 @@
-const app = (require('express'))();
+const express = require('express');
+const app = express();
+
+require('./data/reddit-db')
+
+const bodyParser = require('body-parser');
 
 //We're using Handlebars for ExpressJS
 var exphbs = require('express-handlebars');
@@ -6,35 +11,16 @@ app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 // In order to parse text (e.g. text from when a user creates a new post)
-const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-// We're using MongoDB as our database
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/reddit-clone');
-mongoose.connection.on('error', console.error.bind(console, "MongoDB connection error: "));
-mongoose.set('debug', true);
+
 
 // The data structure for Post
 const Post = require('./models/post');
 
 //GET and POST method for creating a new post
 require('./controllers/posts.js')(app);
-
-
-// Home page
-app.get('/', (req, res) => {
-  Post.find({}).then((posts) => {
-    res.render('home', { posts });
-
-  }).catch((err) => {
-    console.log(err.message);
-  });
-});
-
-
 
 //The port for this website
 app.listen(3000, () => {
